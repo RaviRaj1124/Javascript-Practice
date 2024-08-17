13a29f595d845b48b794e1574b9c85c5c59639fc
RaviRaj1124 <ravirajshinge1124@gmail.com>
2024-08-17 13:03:02 +0530

    created permitive Datatypes

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 console.log("Hello JAvascript");
 
-alert("hello");
-
-
 //let veriable
 let name = "Raviraj";
-console.log(name);
+console.log("Name =",name);
 
+//Constant Varibale
+const intrestRate = 2;
+console.log("Intrest Rate =",intrestRate);
+
+//primitive Datatypes
+/*
+1.strings
+2.Boolean
+3.Numbers
+4.undefined
+5.null
+*/
+
+let uName = "Raviraj";//string
+let age = 22;//number
+let isVoter = true;// boolean
+let fName; //undefined
+let lName  = null; // null 
